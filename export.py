e1012c55b117474415ea8b43efc2e0db583bc00a
Gergo Bereczk <bereczkt.gergo@gmail.com>
2018-11-16 19:49:00 +0100

    give menu for

--- export.py
+++ export.py
@@ -12,8 +12,78 @@ def export(resolution):
             k.write (resolution)
             k.write("\n") 
 
-export(str(reports.count_games("/home/gergo/Desktop/5 th week/game_stat.txt")))
-export(str(reports.decide(2009, "/home/gergo/Desktop/5 th week/game_stat.txt")))
-export(str(reports.get_latest("/home/gergo/Desktop/5 th week/game_stat.txt")))
-export(str(reports.count_by_genre("/home/gergo/Desktop/5 th week/game_stat.txt", "First-person shooter")))
-export(str(reports.get_line_number_by_title("/home/gergo/Desktop/5 th week/game_stat.txt", "Diablo III")))
+# export(str(reports.count_games("/home/gergo/Desktop/5 th week/game_stat.txt")))
+# export(str(reports.decide(2009, "/home/gergo/Desktop/5 th week/game_stat.txt")))
+# export(str(reports.get_latest("/home/gergo/Desktop/5 th week/game_stat.txt")))
+# export(str(reports.count_by_genre("/home/gergo/Desktop/5 th week/game_stat.txt", "First-person shooter")))
+# export(str(reports.get_line_number_by_title("/home/gergo/Desktop/5 th week/game_stat.txt", "Diablo III")))
+
+
+def menu():
+    switch_on = 1
+    while switch_on == 1:
+
+        mode = input("Please give me a number of the function:")
+
+        if mode in "1234567":
+            if mode == "1":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    export(str(reports.count_games(information1)))
+                except FileNotFoundError:
+                    print ("Invalid input, please try again")
+                    continue
+            if mode == "2":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    export(str(reports.get_latest(information1)))
+                except FileNotFoundError:
+                    print ("Invalid input, please try again")
+                    continue
+            if mode == "3":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    information2 = input("Please give me the genre:")
+                    export(str(reports.count_by_genre(information1, information2)))
+                except FileNotFoundError:
+                    print ("Invalid input, please try again")
+                    continue
+            if mode == "4":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    information2 = input("Please give me the title:")
+                    export(str(reports.get_line_number_by_title(information1, information2)))
+                except FileNotFoundError:
+                    print ("Invalid input, please try again")
+                    continue
+            if mode == "5":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    export(str(reports.sort_abc(information1)))
+                except:
+                    print ("Invalid number, please try again")
+                    continue
+
+            if mode == "6":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    export(str(reports.get_genres(information1)))
+                except:
+                    print ("Invalid number, please try again")
+                    continue
+            
+            if mode == "7":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    export(str(reports.when_was_top_sold_fps(information1)))
+                except:
+                    print ("Invalid number, please try again")
+                    continue
+            
+            
+            exitQuestion = input("If would you like to ask another question please press 1 button and for exit push any other button: ")
+            if exitQuestion != "1":
+                switch_on += 1
+
+
+menu()
--- printing.py
+++ printing.py
@@ -1,8 +1,71 @@
 import reports
 # Printing functions
 
-print (reports.count_games("/home/gergo/Desktop/5 th week/game_stat.txt"))
-print (reports.decide("/home/gergo/Desktop/5 th week/game_stat.txt",2000))
-print (reports.get_latest("/home/gergo/Desktop/5 th week/game_stat.txt"))
-print (reports.count_by_genre("/home/gergo/Desktop/5 th week/game_stat.txt", "First-person shooter"))
-print (reports.get_line_number_by_title("/home/gergo/Desktop/5 th week/game_stat.txt", "Diablo III"))
+def menu():
+    switch_on = 1
+    while switch_on == 1:
+
+        mode = input("Please give me a number of the function:")
+
+        if mode in "1234567":
+            if mode == "1":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    print (reports.count_games(information1))
+                except FileNotFoundError:
+                    print ("Invalid input, please try again")
+                    continue
+            if mode == "2":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    print (reports.get_latest(information1))
+                except FileNotFoundError:
+                    print ("Invalid input, please try again")
+                    continue
+            if mode == "3":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    information2 = input("Please give me the genre:")
+                    print (reports.count_by_genre(information1, information2))
+                except FileNotFoundError:
+                    print ("Invalid input, please try again")
+                    continue
+            if mode == "4":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    information2 = input("Please give me the title:")
+                    print (reports.get_line_number_by_title(information1, information2))
+                except FileNotFoundError:
+                    print ("Invalid input, please try again")
+                    continue
+            if mode == "5":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    print (reports.sort_abc(information1))
+                except:
+                    print ("Invalid number, please try again")
+                    continue
+
+            if mode == "6":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    print (reports.get_genres(information1))
+                except:
+                    print ("Invalid number, please try again")
+                    continue
+            
+            if mode == "7":
+                try:
+                    information1 = input("Please give me the name of the txt file:")
+                    print (reports.when_was_top_sold_fps(information1))
+                except:
+                    print ("Invalid number, please try again")
+                    continue
+            
+            
+            exitQuestion = input("If would you like to ask another question please press 1 button and for exit push any other button: ")
+            if exitQuestion != "1":
+                switch_on += 1
+
+
+menu()
